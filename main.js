286c2a6202b4078da4d55e4d3ae293afff5db7d7
adeka stanley <adekastanley2@gmail.com>
2023-04-26 22:02:58 +0100

    working on mobile animations

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-alert(1)
+const hamburger = document.querySelector('.ham')
+
+// const target = document.querySelector('.main')
+// const result = splitting({target:target, by: 'chars'})
+
+
+let navOpen = false;
+hamburger.addEventListener('click', ()=> {
+    if (!navOpen){
+
+        gsap.to('header', {
+            x : 0,
+            ease: 'power4'
+        })
+        navOpen = true
+    } else {
+        gsap.to('header', {
+            x : '100%',
+            ease: 'power4'
+        })
+        navOpen = false
+    }
+})
