c8ad6ff66dea46df08124c7aac3169556aa9058e
adeka stanley <adekastanley2@gmail.com>
2023-04-26 22:33:29 +0100

    safety commit

--- main.js
+++ main.js
@@ -3,20 +3,28 @@ const hamburger = document.querySelector('.ham')
 // const target = document.querySelector('.main')
 // const result = splitting({target:target, by: 'chars'})
 
+const mobNavAnim = gsap.timeline()
 
 let navOpen = false;
 hamburger.addEventListener('click', ()=> {
     if (!navOpen){
 
-        gsap.to('header', {
+        mobNavAnim.to('header', {
             x : 0,
             ease: 'power4'
+        })
+        mobNavAnim.from('.nav-links', {
+            ease: 'power4',
+            duration: .5,
+            stagger: 0.5,
+            opacity: 0,
+            y: -10,
         })
         navOpen = true
     } else {
         gsap.to('header', {
             x : '100%',
-            ease: 'power4'
+            ease: 1
         })
         navOpen = false
     }
